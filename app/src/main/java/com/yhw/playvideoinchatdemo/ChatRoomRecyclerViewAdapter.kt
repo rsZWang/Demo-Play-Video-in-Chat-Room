09e43f87559e003c22e0f969cb18252f4a05ace9
harry <harry@rooit.me>
2021-11-08 18:55:06 +0800

    finished

--- app/src/main/java/com/yhw/playvideoinchatdemo/ChatRoomRecyclerViewAdapter.kt
+++ app/src/main/java/com/yhw/playvideoinchatdemo/ChatRoomRecyclerViewAdapter.kt
@@ -1,15 +1,27 @@
 package com.yhw.playvideoinchatdemo
 
+import android.content.res.Resources
+import android.media.MediaMetadataRetriever
+import android.net.Uri
+import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.yhw.playvideoinchatdemo.databinding.ViewHolderChatRoomTextBinding
 import com.yhw.playvideoinchatdemo.databinding.ViewHolderChatRoomVideoBinding
+import android.util.DisplayMetrics
+import android.R.string.no
+import android.util.TypedValue
+import java.io.File
 
 class ChatRoomRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     companion object {
+        private const val TAG = "ChatRoomRecyclerViewAdapter"
         private const val TEXT = 1
         private const val PICTURE = 2
         private const val VIDEO = 3
@@ -19,8 +31,7 @@ class ChatRoomRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     fun add(message: MessageType) {
         messageTypeList.add(message)
-        notifyDataSetChanged()
-//        notifyItemInserted(messageTypeList.size+1)
+        notifyItemInserted(messageTypeList.size+1)
     }
 
     override fun getItemCount(): Int = messageTypeList.size
@@ -54,8 +65,53 @@ class ChatRoomRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     private inner class VideoViewHolder(val binding: ViewHolderChatRoomVideoBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(text: MessageType) {
+            val uriString = (text as MessageVideo).videoUri
+            val uri = Uri.fromFile(File(uriString))
 
+            val retriever = MediaMetadataRetriever()
+            retriever.setDataSource(itemView.context, uri)
+            var videoWidth = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)!!.toFloat()
+            var videoHeight = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)!!.toFloat()
+            retriever.release()
+
+            val displayMetrics: DisplayMetrics = Resources.getSystem().displayMetrics
+            val maxWidth = ((displayMetrics.widthPixels.toFloat()/3)*2)
+            if (videoWidth > maxWidth) {
+                val scale = maxWidth/videoWidth
+                videoWidth = maxWidth
+                videoHeight *= scale
+            }
+
+            val newLayoutParams = binding.videoView.layoutParams
+            newLayoutParams.width *= videoWidth.toInt()
+            newLayoutParams.height *= videoHeight.toInt()
+            Log.i("ADAPTER", "width: ${newLayoutParams.width}")
+            Log.i("ADAPTER", "height: ${newLayoutParams.height}")
+            binding.videoView.requestLayout()
+            binding.videoView.setOnPreparedListener {
+                binding.videoView.start()
+            }
+            var counter = 0
+            val max = 2
+            binding.videoView.setOnCompletionListener {
+                if (counter < max) {
+                    counter += 1
+                    binding.videoView.seekTo(0)
+                    binding.videoView.start()
+                }
+            }
+            binding.videoView.setVideoURI(uri)
         }
     }
+
+    val Int.dp: Int
+        get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+    val Int.px: Int
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+    val Float.dp: Float
+        get() = (this / Resources.getSystem().displayMetrics.density)
+    val Float.px: Float
+        get() = (this * Resources.getSystem().displayMetrics.density)
 
 }
--- app/src/main/java/com/yhw/playvideoinchatdemo/MainActivity.kt
+++ app/src/main/java/com/yhw/playvideoinchatdemo/MainActivity.kt
@@ -1,42 +1,96 @@
 package com.yhw.playvideoinchatdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.lassi.common.utils.KeyUtils
+import com.lassi.data.media.MiMedia
+import com.lassi.domain.media.LassiOption
+import com.lassi.domain.media.MediaType
+import com.lassi.presentation.builder.Lassi
 import com.yhw.playvideoinchatdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val TAG = "MainActivity"
+        private const val TAG = "MainActivity"
     }
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewAdapter = ChatRoomRecyclerViewAdapter()
+
+    private lateinit var receiveData: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val viewAdapter = ChatRoomRecyclerViewAdapter()
 
         with(binding.chatRecyclerView) {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = viewAdapter
         }
 
+        binding.albumButton.setOnClickListener {
+            setupPicker()
+        }
+
         binding.sendButton.setOnClickListener {
-            Log.i(TAG, "SEND!!")
             val text = binding.textEditText.text.toString()
             if (text.isNotEmpty()) {
                 viewAdapter.add(MessageText(text))
                 binding.textEditText.text?.clear()
+                (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                    .hideSoftInputFromWindow(binding.textEditText.windowToken, 0)
             }
         }
 
+        receiveData = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val selectedMedia = it.data?.getSerializableExtra(KeyUtils.SELECTED_MEDIA) as ArrayList<MiMedia>
+                if (!selectedMedia.isNullOrEmpty()) {
+                    viewAdapter.add(MessageVideo(selectedMedia[0].path!!))
+                }
+            }
+        }
+    }
 
+    private fun setupPicker() {
+
+        val intent = Lassi(this)
+            .with(LassiOption.CAMERA_AND_GALLERY) // choose Option CAMERA, GALLERY or CAMERA_AND_GALLERY
+            .setMaxCount(5)
+            .setGridSize(3)
+            .setMediaType(MediaType.VIDEO) // MediaType : VIDEO IMAGE, AUDIO OR DOC
+//            .setCompressionRation(10) // compress image for single item selection (can be 0 to 100)
+//            .setMinTime(15) // for MediaType.VIDEO only
+//            .setMaxTime(30) // for MediaType.VIDEO only
+//            .setSupportedFileTypes("mp4", "mkv", "webm", "avi", "flv", "3gp") // Filter by limited media format (Optional)
+//            .setMinFileSize(100) // Restrict by minimum file size
+//            .setMaxFileSize(1024) //  Restrict by maximum file size
+            .disableCrop() // to remove crop from the single image selection (crop is enabled by default for single image)
+            /*
+             * Configuration for  UI
+             */
+//            .setStatusBarColor(R.color.colorPrimaryDark)
+//            .setToolbarResourceColor(R.color.colorPrimary)
+//            .setProgressBarColor(R.color.colorAccent)
+//            .setPlaceHolder(R.drawable.ic_image_placeholder)
+//            .setErrorDrawable(R.drawable.ic_image_placeholder)
+//            .setSelectionDrawable(R.drawable.ic_checked_media)
+//            .setCropType(CropImageView.CropShape.RECTANGLE) // choose shape for cropping after capturing an image from camera (for MediaType.IMAGE only)
+//            .setCropAspectRatio(1, 1) // define crop aspect ratio for cropping after capturing an image from camera (for MediaType.IMAGE only)
+//            .enableFlip() // Enable flip image option while image cropping (for MediaType.IMAGE only)
+//            .enableRotate() // Enable rotate image option while image cropping (for MediaType.IMAGE only)
+            .enableActualCircleCrop() // Enable actual circular crop (only for MediaType.Image and CropImageView.CropShape.OVAL)
+            .build()
+
+        receiveData.launch(intent)
     }
 
 }
